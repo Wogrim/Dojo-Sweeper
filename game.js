831f39de8aa5f6e4249a969ba556e5ceb3851c5f
Tobias Carothers <wogrim.pseul@gmail.com>
2022-07-13 22:18:42 -0700

    reorganized board-related variables into an object

--- game.js
+++ game.js
@@ -1,6 +1,12 @@
 //theDojo is a 2D array of objects ("squares") which keep track of their status
-var theDojo = [];
-var dojoDiv = document.querySelector("#the-dojo");
+var theDojo = 
+{
+  width: 0,
+  height: 0,
+  ninjas: 0,
+  squares: [],
+  div: document.querySelector("#the-dojo")
+};
 
 var width_input = document.querySelector("#width");
 var height_input = document.querySelector("#height");
@@ -14,14 +20,14 @@ function makeSquare(i, j) {
     nearby: 0,
     btn: 0,
     status: 0
-    //status is default 0
+    //0 regular
     //1 revealed
     //2 marked
   }
 }
 
 function leftclick(i, j) {
-  var square = theDojo[i][j];
+  var square = theDojo.squares[i][j];
 
   // if the square is marked or already revealed, don't do anything
   if (square.status > 0)
@@ -43,19 +49,19 @@ function leftclick(i, j) {
   // by recursively calling the leftclick() function
   if (square.nearby === 0) {
     var imin = Math.max(0, i - 1);
-    var imax = Math.min(theDojo.length - 1, i + 1);
+    var imax = Math.min(theDojo.height - 1, i + 1);
     var jmin = Math.max(0, j - 1);
-    var jmax = Math.min(theDojo[i].length - 1, j + 1);
+    var jmax = Math.min(theDojo.width - 1, j + 1);
 
     for (var i2 = imin; i2 <= imax; i2++)
       for (var j2 = jmin; j2 <= jmax; j2++)
-        if (theDojo[i2][j2].status === 0)
+        if (theDojo.squares[i2][j2].status === 0)
           leftclick(i2, j2);
   }
 }
 
 function rightclick(i, j) {
-  var square = theDojo[i][j];
+  var square = theDojo.squares[i][j];
   //don't let the user mark buttons that are already revealed
   if (square.status === 1)
     return;
@@ -74,13 +80,15 @@ function rightclick(i, j) {
 }
 
 function randomBoard(width, height, ninjas) {
-  // console.log(`making board with ${width} width, ${height} height, ${ninjas} ninjas`);
+  theDojo.width=width;
+  theDojo.height=height;
+  theDojo.ninjas=ninjas;
   // create the empty board
-  theDojo = [];
+  theDojo.squares = [];
   for (var i = 0; i < height; i++) {
-    theDojo.push([])
+    theDojo.squares.push([])
     for (var j = 0; j < width; j++)
-      theDojo[i].push(makeSquare(i, j));
+      theDojo.squares[i].push(makeSquare(i, j));
   }
 
   // set {ninjas} random spots on the board to 1
@@ -88,11 +96,11 @@ function randomBoard(width, height, ninjas) {
   var count = 0;
   while (count < ninjas) {
     // get location of a random spot on the board
-    var i = Math.floor(Math.random() * theDojo.length);
-    var j = Math.floor(Math.random() * theDojo[i].length);
+    var i = Math.floor(Math.random() * height);
+    var j = Math.floor(Math.random() * width);
     // if the spot is empty, fill it, otherwise try again
-    if (!theDojo[i][j].ninja) {
-      theDojo[i][j].ninja = true;
+    if (!theDojo.squares[i][j].ninja) {
+      theDojo.squares[i][j].ninja = true;
       count++;
     }
   }
@@ -101,29 +109,29 @@ function randomBoard(width, height, ninjas) {
   for (var i = 0; i < height; i++)
     for (var j = 0; j < width; j++) {
       var imin = Math.max(0, i - 1);
-      var imax = Math.min(theDojo.length - 1, i + 1);
+      var imax = Math.min(height - 1, i + 1);
       var jmin = Math.max(0, j - 1);
-      var jmax = Math.min(theDojo[i].length - 1, j + 1);
+      var jmax = Math.min(width - 1, j + 1);
       for (var i2 = imin; i2 <= imax; i2++)
         for (var j2 = jmin; j2 <= jmax; j2++)
-          if (theDojo[i2][j2].ninja)
-            theDojo[i][j].nearby++;
+          if (theDojo.squares[i2][j2].ninja)
+            theDojo.squares[i][j].nearby++;
     }
 
   // set width of the board so the buttons wrap properly (buttons are 32px)
-  dojoDiv.style.width = width * 32 + "px";
+  theDojo.div.style.width = width * 32 + "px";
 
   //create the board buttons
   buttonshtml = "";
   for (var i = 0; i < height; i++)
     for (var j = 0; j < width; j++)
       buttonshtml += `<button onclick="leftclick(${i}, ${j})" oncontextmenu="rightclick(${i}, ${j});return false;"></button>`;
-  dojoDiv.innerHTML = buttonshtml;
+  theDojo.div.innerHTML = buttonshtml;
 
   //save the references to the board buttons
   for (var i = 0; i < height; i++)
     for (var j = 0; j < width; j++)
-      theDojo[i][j].btn = document.querySelector(`#the-dojo > button:nth-child(${i * height + j + 1})`);
+      theDojo.squares[i][j].btn = document.querySelector(`#the-dojo > button:nth-child(${i * width + j + 1})`);
 
 }
 
